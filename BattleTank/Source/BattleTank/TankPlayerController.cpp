30615d546031bec6fa7af856fde6fe7536d25428
cebli <iletisim@cebli.com>
2019-04-12 15:12:44 +0300

    Ders 147

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,11 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
+void  ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
 
 void ATankPlayerController::BeginPlay()
 {
@@ -22,4 +27,27 @@ void ATankPlayerController::BeginPlay()
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void  ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank())	{return;}
+
+	FVector HitLocation; // Our parameter
+	
+	// Return an OUT Parameter, true if hit landscape 
+
+	if (GetSightRayHitLocation(HitLocation))
+	{	
+	UE_LOG(LogTemp, Warning, TEXT("Hitlocation : %s"), *HitLocation.ToString())
+	}
+	// 
+		// TODO Tell controlled tank to aim at this point
+}
+
+// Get World location if linetrace through crosshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& Hitlocation) const
+{
+	Hitlocation = FVector(1.f);
+	return true;
 }
